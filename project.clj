b475817cbac061a7b4f23bbe12cbd6e190aa6aad
eric feigner <eric@weareinstrument.com>
2012-05-02 13:49:26 -0700

    adding local snapshot / release repo for CI

--- project.clj
+++ project.clj
@@ -10,4 +10,9 @@
                  [org.clojure/tools.logging "0.2.3"]]
                  ;; [antler/clojure-solr "0.3.0-SNAPSHOT"]
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
-  :aot [caribou.model])
+  :aot [caribou.model]
+  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"
+                 "snapshots" {:url "http://battlecat:8080/nexus/content/repositories/snapshots" 
+                              :username "deployment" :password "deployment"}
+                 "releases"  {:url "http://battlecat:8080/nexus/content/repositories/releases" 
+                              :username "deployment" :password "deployment"}})
