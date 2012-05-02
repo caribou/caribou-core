d4180444d15723d2f7b3117095c93c33b41bee15
Ryan Roemmich <ryan@weareinstrument.com>
2012-05-02 11:32:13 -0700

    remove config dir from core

--- config/development.clj
+++
@@ -1,7 +0,0 @@
-{:database {:classname    "org.postgresql.Driver"
-            :subprotocol  "postgresql"
-            :host         "localhost"
-            :database     "caribou_development"
-            :user         "postgres"
-            :password     ""} 
- :controller-ns  "skel.controllers"}
--- config/test.clj
+++
@@ -1,7 +0,0 @@
-{:database {:classname    "org.postgresql.Driver"
-            :subprotocol  "postgresql"
-            :host         "localhost"
-            :database     "caribou_test"
-            :user         "postgres"
-            :password     ""}
- :asset-dir "assets"}
