dda9c2f33cfd491ee661a42e7929c7b39747571a
Justin Glenn Smith <noisesmith@gmail.com>
2012-07-12 14:50:12 -0700

    initial setup of dynamic logging configuration for caribou

--- src/caribou/logger.clj
+++ src/caribou/logger.clj
@@ -1,22 +1,47 @@
 (ns caribou.logger
-  (:require [clojure.tools.logging :as logging]))
+  (:require [clj-logging-config.log4j :as logconf]
+            [clojure.tools.logging :as logging]))
+
+(defmacro with-config
+  "Use our explicit configuration while logging something, with
+  dynamic additions in a map. Explicitly using with-config on every
+  logging call avoids some pervasive weirdness with configuration
+  clobbering."
+  [map & body]
+  `(logconf/with-logging-config
+     [:root (merge
+             {:level (:log-level @config/app)
+              :layout (:log-layout @config/app)
+              :filter (:log-filter @config/app)}
+             ~map)]
+     ~@body))
 
 (defn debug 
   "Log a debug message (with an optional prefix)"
-  ([msg] (logging/debug msg))
+  ([msg] (with-config {} (logging/debug msg)))
   ([msg prefix] (debug (str (name prefix) ": " msg))))
 
 (defn info 
   "Log an info message (with an optional prefix)"
-  ([msg] (logging/info msg))
+  ([msg] (with-config {} (logging/info msg)))
   ([msg prefix] (info (str (name prefix) ": " msg))))
 
 (defn warn 
   "Log a warning message (with an optional prefix)"
-  ([msg] (logging/warn msg))
+  ([msg] (with-config {} (logging/warn msg)))
   ([msg prefix] (warn (str (name prefix) ": " msg))))
 
 (defn error 
   "Log an error message (with an optional prefix)"
-  ([msg] (logging/error msg))
+  ([msg] (with-config {} (logging/error msg)))
   ([msg prefix] (error (str (name prefix) ": " msg))))
+
+(defmacro spy
+  "Spy the value of an expression (with an optional prefix)"
+  ([form] `(logconf/with-logging-config {} (logging/spy '~form)))
+  ([form prefix] `(logconf/with-logging-config
+                    {:log-layout (org.apache.log4j.PatternLayout.
+                                  (str prefix " "
+                                       (.getConversionPattern
+                                        (:log-layout @config/app))))}
+                    (logging/spy '~form))))
