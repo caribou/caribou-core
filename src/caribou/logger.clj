3a45cf325fbf8e469573f1ab3a3110de9968d2aa
Justin Glenn Smith <noisesmith@gmail.com>
2012-08-01 10:43:46 -0700

    still working on messages

--- src/caribou/logger.clj
+++ src/caribou/logger.clj
@@ -15,46 +15,58 @@
                        :level (:log-level @defaults)
                        :filter (:log-filter @defaults)))
 
+(defn replace-strings
+  "helper to simplify capture-pattern-context"
+  [str rep]
+  (clojure.string/replace str (rep 0) (rep 1)))
+
+(defmacro capture-pattern-context
+  [pattern]
+  `(reduce
+    replace-strings
+    pattern
+    [;; location
+     [#"%l" "%M : %F (%L)"]
+     ;; method (function?), cannot find this atm
+     [#"%M" ""]
+     ;; file name
+     [#"%F" ~*file*]
+     ;; line number
+     [#"%L" (str ~(:line (meta &form)))]]))
+
 (defmacro with-config
   "Use our explicit configuration while logging something, with
   dynamic additions in a map. Explicitly using with-config on every
   logging call avoids some pervasive weirdness with configuration
   clobbering."
   [map & body]
-  `(logconf/with-logging-config
-     [:root (merge
-             {:level (:log-level @defaults)
-              ;; pattern is done this way because the java logging tools
-              ;; don't understand where we are in clojure land
-              :pattern (-> (:log-pattern @defaults)
-                           ;; location
-                           #(clojure.string/replace % #"%l" "%M : %F (%L)")
-                           ;; method (function?), cannot find this atm
-                                        ;#(clojure.string/replace % #"%M" "")
-                           (clojure.string/replace % #"%F" ~*file*)
-                           (clojure.string/replace % #"%L"
-                                                   ~(:line (meta &form)))))
-;              (:log-pattern @defaults)
-              :filter (:log-filter @defaults)}
-             ~map)]
-     ~@body))
+  `(let [#config (merge
+                  {:level (:log-level @defaults)
+                   :pattern (:log-pattern @defaults)
+                   :filter (:log-filter @defaults)}
+                  ~map)
+         #config (update-in #config [:pattern]
+                            capture-pattern-context)]
+     (logconf/with-logging-config
+       [:root #config]
+       ~@body)))
 
-(defmacro debug 
+(defmacro debug
   "Log a debug message (with an optional prefix)"
   ([msg] `(with-config {} (logging/debug ~msg)))
   ([msg prefix] `(debug (str (name ~prefix) ": " ~msg))))
 
-(defn info 
+(defmacro info 
   "Log an info message (with an optional prefix)"
   ([msg] `(with-config {} (logging/info ~msg)))
   ([msg prefix] `(info (str (name ~prefix) ": " ~msg))))
 
-(defn warn 
+(defmacro warn 
   "Log a warning message (with an optional prefix)"
   ([msg] `(with-config {} (logging/warn ~msg)))
   ([msg prefix] `(warn (str (name ~prefix) ": " ~msg))))
 
-(defn error 
+(defmacro error 
   "Log an error message (with an optional prefix)"
   ([msg] `(with-config {} (logging/error ~msg)))
   ([msg prefix] `(error (str (name ~prefix) ": " ~msg))))
