12530736a5ce8c8c5b24039b5bfb634baaf8b1d6
Justin Glenn Smith <noisesmith@gmail.com>
2012-08-02 11:04:32 -0700

    simplifying logging, and getting more accurate info about location

--- src/caribou/logger.clj
+++ src/caribou/logger.clj
@@ -15,24 +15,17 @@
                        :level (:log-level @defaults)
                        :filter (:log-filter @defaults)))
 
-(defn replace-strings
-  "helper to simplify capture-pattern-context"
-  [str rep]
-  (clojure.string/replace str (rep 0) (rep 1)))
-
-(defmacro capture-pattern-context
-  [pattern]
-  `(reduce
-    replace-strings
-    pattern
-    [;; location
-     [#"%l" "%M : %F (%L)"]
-     ;; method (function?), cannot find this atm
-     [#"%M" ""]
-     ;; file name
-     [#"%F" ~*file*]
-     ;; line number
-     [#"%L" (str ~(:line (meta &form)))]]))
+(defn unwrap-conf
+  "this avoids some issues with macro-expansion in with-config"
+  [file line]
+  (fn [pattern]
+    (clojure.string/replace
+     (clojure.string/replace pattern; two passes needed here
+                             "%l" "%F : %M (%L)") ; since this one is recursive
+     #"%M|%F|%L"
+     {"%M" "" ; dunno how to find method / function yet, %M just gives us "write_BANG_"
+      "%F" ((fnil identity "") file) ; file will be a string or nil
+      "%L" (str line)}))) ; line will be a number or nil
 
 (defmacro with-config
   "Use our explicit configuration while logging something, with
@@ -40,42 +33,49 @@
   logging call avoids some pervasive weirdness with configuration
   clobbering."
   [map & body]
-  `(let [#config (merge
-                  {:level (:log-level @defaults)
-                   :pattern (:log-pattern @defaults)
-                   :filter (:log-filter @defaults)}
-                  ~map)
-         #config (update-in #config [:pattern]
-                            capture-pattern-context)]
+  `(let [customized# (merge ~map @defaults)
+         base# (merge {:level (:log-level customized#)
+                       :pattern (:log-pattern customized#)
+                       :filter (:log-filter customized#)}
+                      ;; merging this again because it may have keys not
+                      ;; caught above
+                      ~map)
+         total# (update-in base# [:pattern]
+                           (unwrap-conf (:file ~map) (:line ~map)))]
      (logconf/with-logging-config
-       [:root #config]
+       [:root total#]
        ~@body)))
 
 (defmacro debug
   "Log a debug message (with an optional prefix)"
-  ([msg] `(with-config {} (logging/debug ~msg)))
+  ([msg] `(with-config {:line ~(:line (meta &form)) :file ~*file*}
+            (logging/debug ~msg)))
   ([msg prefix] `(debug (str (name ~prefix) ": " ~msg))))
 
-(defmacro info 
+(defmacro info
   "Log an info message (with an optional prefix)"
-  ([msg] `(with-config {} (logging/info ~msg)))
+  ([msg] `(with-config {:line ~(:line (meta &form)) :file ~*file*}
+            (logging/info ~msg)))
   ([msg prefix] `(info (str (name ~prefix) ": " ~msg))))
 
-(defmacro warn 
+(defmacro warn
   "Log a warning message (with an optional prefix)"
-  ([msg] `(with-config {} (logging/warn ~msg)))
+  ([msg] `(with-config {:line ~(:line (meta &form)) :file ~*file*}
+            (logging/warn ~msg)))
   ([msg prefix] `(warn (str (name ~prefix) ": " ~msg))))
 
 (defmacro error 
   "Log an error message (with an optional prefix)"
-  ([msg] `(with-config {} (logging/error ~msg)))
+  ([msg] `(with-config {:line ~(:line (meta &form)) :file ~*file*}
+            (logging/error ~msg)))
   ([msg prefix] `(error (str (name ~prefix) ": " ~msg))))
 
 (defmacro spy
   "Spy the value of an expression (with an optional prefix)"
-  ([form] `(logconf/with-logging-config {} (logging/spy '~form)))
+  ([form] `(logconf/with-logging-config {:line ~(:line (meta &form)) :file ~*file*}
+             (logging/spy '~form)))
   ([form prefix] `(logconf/with-logging-config
                     {:pattern 
-                     (str prefix " "
+                     (str ~prefix " "
                           (:log-pattern @config/app))}
                     (logging/spy '~form))))
