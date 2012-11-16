6f3c89249026f843a03498b7af89dd553f5bec26
Ryan Roemmich <ralfonso@gmail.com>
2012-11-16 16:43:19 -0700

    pedantic deps. 0.7.13

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/caribou-core "0.7.12"
+(defproject antler/caribou-core "0.7.13"
   :description "Caribou is a dynamic web application generator with antlers."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/java.jdbc "0.2.3"]
