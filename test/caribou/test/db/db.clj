51fe8f5da0708210146674c4d17f6d4864a37246
Justin Glenn Smith <noisesmith@gmail.com>
2013-02-13 14:32:22 -0800

    tagging of tests by backend

--- test/caribou/test/db/db.clj
+++ test/caribou/test/db/db.clj
@@ -1,14 +1,12 @@
 (ns caribou.test.db.db
   (:use [clojure.test]
-        [caribou.util])
+        [caribou.util]
+        [caribou.db.adapter.protocol :as adapter])
   (:require [clojure.java.jdbc :as sql]
             [clojure.java.io :as io]
             [caribou.config :as config]
+            [clojure.string :as string]
             [caribou.db :as db]))
-
-(def test-config (config/read-config (io/resource "config/test.clj")))
-
-(config/configure test-config)
 
 (defn db-fixture
   [f]
@@ -18,35 +16,70 @@
       (when (db/table? tmp-table)
         (db/drop-table tmp-table)))))
 
-(use-fixtures :each db-fixture)
+(defn unicode-support-test
+  []
+  (is (adapter/unicode-supported? @config/db-adapter)))
 
 ;; TODO: test database...
 ;; query
-(deftest query-test
+(defn query-test
+  []
   (let [q (query "select * from model")]
     (is (not (empty? q)))))
 
-(deftest query2-exception-test
-  (is (thrown-with-msg? Exception #"relation .* does not exist"
-        (query "select * from modelz"))))
+(defn query2-exception-test
+  []
+  (let [unknown-regexes ["relation .* does not exist"
+                         "Table .* doesn't exist"]
+        unknown-re (string/join "|" unknown-regexes)]
+    (is (thrown-with-msg? Exception (re-pattern unknown-re)
+          (query "select * from modelz")))))
 
 ;; TODO: insert / update / delete / fetch
 
-(deftest choose-test
+(defn choose-test
+  []
   (is (= (get (db/choose :model 1) :name) "Model"))
   (is (= (get (db/choose :model 0) :name) nil)))
 
-(deftest table-test
+(defn table-test
+  []
   (is (db/table? "model"))
   (is (not (db/table? "modelzzzz"))))
 
-(deftest create-new-table-drop-table-test
+(defn create-new-table-drop-table-test
+  []
   (let [tmp-table "veggies"]
     (db/create-table tmp-table [:id "SERIAL" "PRIMARY KEY"])
     (is (db/table? tmp-table))))
 
 ;; TODO: test to ensure options applied to column
-(deftest add-column-test
+(defn add-column-test
+  []
   (let [tmp-table "fruit"]
     (db/create-table tmp-table [:id "SERIAL" "PRIMARY KEY"])
     (db/add-column tmp-table "jopotonio" [:integer])))
+
+(deftest ^:mysql
+  mysql-tests
+  (let [config (config/read-config (io/resource "config/test-mysql.clj"))]
+    (config/configure config)
+    (db-fixture unicode-support-test)
+    (db-fixture query-test)
+    (db-fixture query2-exception-test)
+    (db-fixture choose-test)
+    (db-fixture table-test)
+    (db-fixture create-new-table-drop-table-test)
+    (db-fixture add-column-test)))
+
+(deftest ^:postgres
+  postgres-tests
+  (let [config (config/read-config (io/resource "config/test-postgres.clj"))]
+    (config/configure config)
+    (db-fixture unicode-support-test)
+    (db-fixture query-test)
+    (db-fixture query2-exception-test)
+    (db-fixture choose-test)
+    (db-fixture table-test)
+    (db-fixture create-new-table-drop-table-test)
+    (db-fixture add-column-test)))
