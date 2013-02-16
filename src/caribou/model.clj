863b156c233b4f904134276b2322368763987bcb
Justin Glenn Smith <noisesmith@gmail.com>
2013-02-16 09:19:47 -0800

    string field namespace

--- src/caribou/model.clj
+++ src/caribou/model.clj
@@ -21,7 +21,8 @@
             ;; namespaces for fields
             caribou.field.id
             caribou.field.integer
-            caribou.field.decimal))
+            caribou.field.decimal
+            caribou.field.string))
 
 (import java.util.Date)
 (import java.text.SimpleDateFormat)
@@ -176,64 +177,6 @@
 (def models (ref {}))
 (def model-slugs (ref {}))
 
-(defn string-where
-  [field prefix slug opts where models]
-  (let [model-id (-> field :row :model_id)
-        model (db/find-model model-id models)
-        [operator value] (field/where-operator where)
-        field-select (field/coalesce-locale model field prefix slug opts)]
-    (clause "%1 %2 '%3'" [field-select operator value])))
-
-(def pool "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ
-           =+!@#$%^&*()_-|:;?/}]{[~` 0123456789")
-
-(defn rand-str
-  ([n] (rand-str n pool))
-  ([n pool]
-     (string/join
-      (map
-       (fn [_]
-         (rand-nth pool))
-       (repeat n nil)))))
-
-(defrecord StringField [row env]
-  field/Field
-  (table-additions [this field] [[(keyword field) "varchar(255)"]])
-  (subfield-names [this field] [])
-  (setup-field [this spec models] nil)
-  (rename-field [this old-slug new-slug])
-  (cleanup-field [this] nil)
-  (target-for [this] nil)
-  (update-values [this content values]
-    (let [key (keyword (:slug row))]
-      (if (contains? content key)
-        (assoc values key (content key))
-        values)))
-
-  (post-update [this content opts] content)
-  (pre-destroy [this content] content)
-  (join-fields [this prefix opts] [])
-  (join-conditions [this prefix opts] [])
-  (build-where
-    [this prefix opts models]
-    (field/field-where this prefix opts string-where models))
-  (natural-orderings [this prefix opts])
-  (build-order [this prefix opts models]
-    (field/pure-order this prefix opts models))
-  (field-generator [this generators]
-    (assoc generators (keyword (:slug row)) (fn [] (rand-str (inc (rand-int 139))))))
-  (fuse-field [this prefix archetype skein opts]
-    (field/pure-fusion this prefix archetype skein opts))
-  (localized? [this] true)
-  (models-involved [this opts all] all)
-  (field-from [this content opts] (content (keyword (:slug row))))
-  (render [this content opts] content)
-  (validate [this opts models] (validation/for-type this opts
-                                                    (fn [s]
-                                                      (or (keyword? s)
-                                                          (string? s)))
-                                                    "string")))
-
 (defrecord PasswordField [row env]
   field/Field
   (table-additions [this field] [[(keyword field) "varchar(255)"]])
@@ -253,7 +196,7 @@
   (join-conditions [this prefix opts] [])
   (build-where
     [this prefix opts models]
-    (field/field-where this prefix opts string-where models))
+    (field/field-where this prefix opts field/string-where models))
   (natural-orderings [this prefix opts])
   (build-order [this prefix opts models]
     (field/pure-order this prefix opts models))
@@ -294,7 +237,7 @@
   (join-conditions [this prefix opts] [])
   (build-where
     [this prefix opts models]
-    (field/field-where this prefix opts string-where models))
+    (field/field-where this prefix opts field/string-where models))
   (natural-orderings [this prefix opts])
   (build-order [this prefix opts models]
     (field/pure-order this prefix opts models))
@@ -340,7 +283,7 @@
   (join-conditions [this prefix opts] [])
   (build-where
     [this prefix opts models]
-    (field/field-where this prefix opts string-where models))
+    (field/field-where this prefix opts field/string-where models))
   (natural-orderings [this prefix opts])
   (build-order [this prefix opts models]
     (field/pure-order this prefix opts models))
@@ -376,7 +319,7 @@
   (join-conditions [this prefix opts] [])
   (build-where
     [this prefix opts models]
-    (field/field-where this prefix opts string-where models))
+    (field/field-where this prefix opts field/string-where models))
   (natural-orderings [this prefix opts])
   (build-order [this prefix opts models]
     (field/pure-order this prefix opts models))
@@ -1900,8 +1843,7 @@
    {:name "Updated At" :slug "updated_at" :type "timestamp" :locked true :editable false}])
 
 (doseq
-    [[key construct] {:string (fn [row] (StringField. row {}))
-                      :slug (fn [row] 
+    [[key construct] {:slug (fn [row] 
                               (let [link (db/choose :field (row :link_id))]
                                 (SlugField. row {:link link})))
                       :urlslug (fn [row] 
