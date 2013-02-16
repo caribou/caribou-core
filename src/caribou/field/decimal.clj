3ef6572033e936ec962942fe914a937b155e1e79
Justin Glenn Smith <noisesmith@gmail.com>
2013-02-16 09:06:05 -0800

    decimal field namespace

---
+++ src/caribou/field/decimal.clj
@@ -0,0 +1,48 @@
+(ns caribou.field.decimal
+  (:require [caribou.field-protocol :as field]
+            [caribou.validation :as validation]))
+
+(defrecord DecimalField [row env]
+  field/Field
+  (table-additions [this field] [[(keyword field) "decimal(20,10)"]])
+  (subfield-names [this field] [])
+  (setup-field [this spec models] nil)
+  (rename-field [this old-slug new-slug])
+  (cleanup-field [this] nil)
+  (target-for [this] nil)
+
+  (update-values [this content values]
+    (let [key (keyword (:slug row))]
+      (if (contains? content key)
+        (try
+          (let [value (content key)
+                tval (if (isa? (type value) String)
+                       (BigDecimal. value)
+                       value)]
+            (assoc values key tval))
+          (catch Exception e values))
+        values)))
+
+  (post-update [this content opts] content)
+  (pre-destroy [this content] content)
+  (join-fields [this prefix opts] [])
+  (join-conditions [this prefix opts] [])
+  (build-where
+    [this prefix opts models]
+    (field/field-where this prefix opts field/pure-where models))
+  (natural-orderings [this prefix opts])
+  (build-order [this prefix opts models]
+    (field/pure-order this prefix opts models))
+  (field-generator [this generators]
+    (assoc generators (keyword (:slug row)) (fn [] (rand))))
+  (fuse-field [this prefix archetype skein opts]
+    (field/pure-fusion this prefix archetype skein opts))
+  (localized? [this] true)
+  (models-involved [this opts all] all)
+  (field-from [this content opts] (content (keyword (:slug row))))
+  (render [this content opts]
+    (update-in content [(keyword (:slug row))] str))
+  (validate [this opts models] (validation/for-type this opts number?
+                                                    "decimal")))
+  
+(field/add-constructor :decimal (fn [row] (DecimalField. row {})))
