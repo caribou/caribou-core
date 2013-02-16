c79c5d6790359ae2f45645a762155f01a3751a38
Justin Glenn Smith <noisesmith@gmail.com>
2013-02-16 14:46:36 -0800

    namespaces for link and part fields

--- src/caribou/field/collection.clj
+++ src/caribou/field/collection.clj
@@ -111,7 +111,8 @@
   (pre-destroy
     [this content]
     (if (or (row :dependent) (-> env :link :dependent))
-      (let [parts (field/field-from this content {:include {(keyword (:slug row)) {}}})
+      (let [parts (field/field-from this content
+                                    {:include {(keyword (:slug row)) {}}})
             target (keyword (get (field/target-for this) :slug))]
         (doseq [part parts]
           ((get @operations :destroy) target (:id part)))))
