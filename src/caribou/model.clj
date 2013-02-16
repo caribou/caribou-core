a6a56c38f8bdbe1f2eb5b6a4fb6a1f794b5bd6cf
Justin Glenn Smith <noisesmith@gmail.com>
2013-02-16 11:08:06 -0800

    slug field namespace

--- src/caribou/model.clj
+++ src/caribou/model.clj
@@ -19,6 +19,7 @@
             caribou.field.string
             caribou.field.password
             caribou.field.text
+            caribou.field.slug
             [caribou.field.time-stamp :as time-stamp-field]))
 
 (defn db
@@ -92,52 +93,6 @@
 
 (def models field/models)
 (def model-slugs (ref {}))
-
-(defrecord SlugField [row env]
-  field/Field
-  (table-additions [this field] [[(keyword field) "varchar(255)"]])
-  (subfield-names [this field] [])
-  (setup-field [this spec] nil)
-  (rename-field [this old-slug new-slug])
-  (cleanup-field [this] nil)
-  (target-for [this] nil)
-  (update-values [this content values]
-    (let [key (keyword (:slug row))]
-      (cond
-       (env :link)
-         (let [icon (content (keyword (-> env :link :slug)))]
-           (if icon
-             (assoc values key (slugify icon))
-             values))
-       (contains? content key) (assoc values key (slugify (content key)))
-       :else values)))
-  (post-update [this content opts] content)
-  (pre-destroy [this content] content)
-  (join-fields [this prefix opts] [])
-  (join-conditions [this prefix opts] [])
-  (build-where
-    [this prefix opts]
-    (field/field-where this prefix opts field/string-where))
-  (natural-orderings [this prefix opts])
-  (build-order [this prefix opts]
-    (field/pure-order this prefix opts))
-  (field-generator [this generators]
-    (assoc generators (keyword (:slug row))
-           (fn []
-             (rand-str
-              (inc (rand-int 139))
-              "_abcdefghijklmnopqrstuvwxyz_"))))
-  (fuse-field [this prefix archetype skein opts]
-    (field/pure-fusion this prefix archetype skein opts))
-  (localized? [this] true)
-  (models-involved [this opts all] all)
-  (field-from [this content opts] (content (keyword (:slug row))))
-  (render [this content opts] content)
-  (validate [this opts] (validation/for-type this opts
-                                             (fn [s]
-                                               (or (keyword? s)
-                                                   (string? s)))
-                                             "slug")))
 
 (defrecord UrlSlugField [row env]
   field/Field
@@ -1617,10 +1572,7 @@
    {:name "Updated At" :slug "updated_at" :type "timestamp" :locked true :editable false}])
 
 (doseq
-    [[key construct] {:slug (fn [row] 
-                              (let [link (db/choose :field (row :link_id))]
-                                (SlugField. row {:link link})))
-                      :urlslug (fn [row] 
+    [[key construct] {:urlslug (fn [row] 
                                  (let [link (db/choose :field (row :link_id))]
                                    (UrlSlugField. row {:link link})))
                       :boolean (fn [row] (BooleanField. row {}))
