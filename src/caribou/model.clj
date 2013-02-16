edc9718c1d703ec5704a293b380a08e9c13f6cd4
Justin Glenn Smith <noisesmith@gmail.com>
2013-02-16 09:06:05 -0800

    decimal field namespace

--- src/caribou/model.clj
+++ src/caribou/model.clj
@@ -20,7 +20,8 @@
             [caribou.auth :as auth]
             ;; namespaces for fields
             caribou.field.id
-            caribou.field.integer))
+            caribou.field.integer
+            caribou.field.decimal))
 
 (import java.util.Date)
 (import java.text.SimpleDateFormat)
@@ -183,50 +184,6 @@
         field-select (field/coalesce-locale model field prefix slug opts)]
     (clause "%1 %2 '%3'" [field-select operator value])))
 
-  
-(defrecord DecimalField [row env]
-  field/Field
-  (table-additions [this field] [[(keyword field) "decimal(20,10)"]])
-  (subfield-names [this field] [])
-  (setup-field [this spec models] nil)
-  (rename-field [this old-slug new-slug])
-  (cleanup-field [this] nil)
-  (target-for [this] nil)
-
-  (update-values [this content values]
-    (let [key (keyword (:slug row))]
-      (if (contains? content key)
-        (try
-          (let [value (content key)
-                tval (if (isa? (type value) String)
-                       (BigDecimal. value)
-                       value)]
-            (assoc values key tval))
-          (catch Exception e values))
-        values)))
-
-  (post-update [this content opts] content)
-  (pre-destroy [this content] content)
-  (join-fields [this prefix opts] [])
-  (join-conditions [this prefix opts] [])
-  (build-where
-    [this prefix opts models]
-    (field/field-where this prefix opts field/pure-where models))
-  (natural-orderings [this prefix opts])
-  (build-order [this prefix opts models]
-    (field/pure-order this prefix opts models))
-  (field-generator [this generators]
-    (assoc generators (keyword (:slug row)) (fn [] (rand))))
-  (fuse-field [this prefix archetype skein opts]
-    (field/pure-fusion this prefix archetype skein opts))
-  (localized? [this] true)
-  (models-involved [this opts all] all)
-  (field-from [this content opts] (content (keyword (:slug row))))
-  (render [this content opts]
-    (update-in content [(keyword (:slug row))] str))
-  (validate [this opts models] (validation/for-type this opts number?
-                                                    "decimal")))
-  
 (def pool "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ
            =+!@#$%^&*()_-|:;?/}]{[~` 0123456789")
 
@@ -1943,8 +1900,7 @@
    {:name "Updated At" :slug "updated_at" :type "timestamp" :locked true :editable false}])
 
 (doseq
-    [[key construct] {:decimal (fn [row] (DecimalField. row {}))
-                      :string (fn [row] (StringField. row {}))
+    [[key construct] {:string (fn [row] (StringField. row {}))
                       :slug (fn [row] 
                               (let [link (db/choose :field (row :link_id))]
                                 (SlugField. row {:link link})))
