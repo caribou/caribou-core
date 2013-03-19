cd2549b4f99fc75fd4fcfc24b0ad9c4bcb04c0e8
ryan spangler <ryan.spangler@gmail.com>
2013-03-19 16:05:41 -0700

    version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/caribou-core "0.9.7"
+(defproject antler/caribou-core "0.9.8"
   :description "Caribou is a dynamic web application generator with antlers."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/java.jdbc "0.2.3"
