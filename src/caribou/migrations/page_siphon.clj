e0b215e5cdbe306508c4349f7ee2fa4dde3a646b
ryan spangler <ryan.spangler@gmail.com>
2013-03-28 16:58:25 -0700

    migrations for siphon

---
+++ src/caribou/migrations/page_siphon.clj
@@ -0,0 +1,24 @@
+(ns caribou.migrations.page-siphon
+  (:require [caribou.config :as config]
+            [caribou.model :as model]
+            [caribou.migrations.bootstrap :as bootstrap]))
+
+(defn migrate
+  []
+  (let [siphon (model/create
+                :model
+                {:name "Siphon"
+                 :description "A specification of content that can be loaded in dynamically."
+                 :locked true
+                 :fields [{:name "Spec" :type "structure"}]})]
+
+    (model/update
+     :model
+     (-> @model/models :page :id)
+     {:fields [{:name "Siphons"
+                :type "collection"
+                :target_id (:id siphon)
+                :map true}]})))
+
+(defn rollback
+  [])
