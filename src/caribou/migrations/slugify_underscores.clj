26e4f331d9f738a7f13ce75f96d95f6505646267
ryan spangler <ryan.spangler@gmail.com>
2013-05-08 15:29:07 -0700

    removed println

--- src/caribou/migrations/slugify_underscores.clj
+++ src/caribou/migrations/slugify_underscores.clj
@@ -11,7 +11,6 @@
             field-slug (-> slug-field :slug util/url-slugify keyword)]
         (doseq [content (db/fetch model-slug "1 = 1")]
           (let [underscored (get content field-slug)
-                _ (println "FIELD SLUG:" field-slug underscored)
                 new-slug (util/url-slugify underscored)]
             (db/update model-slug ["id = ?" (:id content)] {field-slug new-slug})))))))
 
