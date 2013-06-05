8ce9a70c7891eeaafd52ae8fe821a30a45afeccb
Justin Glenn Smith <noisesmith@gmail.com>
2013-06-05 08:31:45 -0700

    a dev version I want in clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/caribou-core "0.11.4"
+(defproject antler/caribou-core "0.11.4-SNAPSHOT"
   :description "Caribou is a dynamic web application generator with antlers."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/java.jdbc "0.2.3"
