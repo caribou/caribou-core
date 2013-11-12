1ddbe27556b0771de639fd7d923395051ecedb99
ryan spangler <ryan.spangler@gmail.com>
2013-11-12 11:48:06 -0800

    removed references to autodoc

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject caribou/caribou-core "0.13.0"
-  :description "Caribou is a dynamic web application generator with antlers."
+  :description "Caribou is a dynamic web application generator with antlers"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/java.jdbc "0.2.3"
                   :exclusions [org.clojure/clojure]]
@@ -32,24 +32,6 @@
             [caribou/lein-caribou "2.4.30"]]
   :repositories [["releases" {:url "https://clojars.org/repo" :creds :gpg}]]
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n" "-Xmx2g"]
-  :autodoc {:name "Caribou Core"
-            :page-title "Caribou Core - Documentation"
-            :description
-          "<p>This library represents the core model and configuration for Caribou.
-           Used alone, it is a powerful way to capture your data model *as data*,
-           effectively shaping it as your application develops.  Once the data
-           model is rendered in this malleable form, Caribou can harness that
-           specification to do a variety of things, including constructing
-           queries based on the relationships *between* models you define.
-           This means it is possible to order or filter results based on
-           conditions that exist in associated models!</p>
-
-           <p>Also, this provides the basis for the other Caribou libraries,
-           <a href=\"http://antler.github.com/caribou-frontend\">Caribou Frontend</a>
-           <a href=\"http://antler.github.com/caribou-api\">Caribou API</a> and
-           <a href=\"http://antler.github.com/caribou-admin\">Caribou Admin</a>,
-           each of which build in their own way upon this data model as a basis."}
-  ;; :aot [caribou.model]
   :test-selectors {:default (constantly true)
                    :mysql :mysql
                    :postgres :postgres
